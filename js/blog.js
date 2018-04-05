dd65c484fd85921c94cb7b25b30010a74bad3ec0
Melissa Moriwaki <Melissa.Moriwaki@LibertyMutual.com>
2018-04-05 15:34:12 -0400

    moving wedding website files to github and off of onedrive

---
+++ js/blog.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+    
+    var width = $(window).width();
+    $(window).resize(function () {
+        if (width <= '768') {
+            $('#products .item').addClass('list-group-item');
+            $('#products .item').removeClass('grid-group-item');
+        }
+    });
+    
+    $('#list').click(function (event) {
+        event.preventDefault();
+        $('#products .item').addClass('list-group-item');
+    });
+    $('#grid').click(function (event) {
+        event.preventDefault();
+        $('#products .item').removeClass('list-group-item');
+        $('#products .item').addClass('grid-group-item');
+    });
+
+
+
+    
+
+    
+});
